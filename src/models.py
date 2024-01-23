a3adc6b2ab8c7854270318551a3e8ecb4ead7e96
Tony1406 <142957069+Tony1406@users.noreply.github.com>
2024-01-23 23:13:05 +0000

    parte 2

--- src/models.py
+++ src/models.py
@@ -1,6 +1,7 @@
-from flask_sqlalchemy import SQLAlchemy, relationship, ForeignKey
+from flask_sqlalchemy import SQLAlchemy #, relationship, ForeignKey
 
 db = SQLAlchemy()
+
 
 class Usuario(db.Model):
     id = db.Column(db.Integer, unique=True, primary_key=True)
@@ -9,21 +10,62 @@ class Usuario(db.Model):
     email = db.Column(db.String(250), unique=True, nullable=False)
     password = db.Column(db.String(250), unique=False, nullable=False)
     subscription_date = db.Column(db.Integer, unique=False, nullable=True)
-    favorito = relationship('Favorito')
+    favorito = db.relationship('Favorito') # agregar db.relationship
+                                           # Favorito con mayuscula
+
+def __repr__(self):                     
+        return '<Usuario %r>' % self.email
+
+    def serialize(self):
+        return {
+              "id": self.id,
+              "name": self.name,
+              "last_name": self.last_name,
+              "email": self.email,
+              "subscription_date": self.subscription_date
+        }
 
 class Personaje(db.Model):
     id = db.Column(db.Integer, unique=True, primary_key=True)
     name = db.Column(db.String(250), unique=False, nullable=False)
     race = db.Column(db.String(250), unique=False, nullable=False)
-    favorito = relationship("favorito")
+    favorito = db.relationship("Favorito")
+
+def __repr__(self):                     
+        return '<Personaje %r>' % self.email
+
+    def serialize(self):
+        return {
+            "id" : self.id,
+            "name" : self.name, 
+            "race" : self.race
+        }
 
 class Planeta(db.Model):
     id = db.Column(db.Integer, unique=True, primary_key=True)
     name = db.Column(db.String(250), unique=False, nullable=False)
     favorito = relationship("favorito")
 
+def __repr__(self):                    
+        return '<Planeta %r>' % self.email
+
+    def serialize(self):
+        return {
+            "id" : self.id,
+            "name" : self.name
+        }
+
 class Favorito(db.Model):
     id = db.Column(db.Integer, unique=True, primary_key=True)
-    id_usuario= db.Column(db.Integer, ForeignKey("usuario.id"))
-    id_planeta= db.Column(db.Integer, ForeignKey("planeta.id"))
-    id_personaje= db.Column(db.Integer, ForeignKey("personaje.id"))
+    id_usuario= db.Column(db.Integer, db.ForeignKey("usuario.id"))
+    id_planeta= db.Column(db.Integer, db.ForeignKey("planeta.id"))
+    id_personaje= db.Column(db.Integer, db.ForeignKey("personaje.id"))
+
+    def __repr__(self):                    
+        return '<Favorito %r>' % self.email
+
+    def serialize(self):
+        return {
+            "id_planeta" : self.id_planeta,
+            "id_personaje" : self.id_personaje
+        }
